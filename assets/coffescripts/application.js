fd99f5fd70535976a81ee991e0b5dd2c7d63b5dc
Alexander Leishman <leishman3@gmail.com>
2014-05-05 01:22:55 -0700

    send ajax on photo addition to form

--- assets/coffescripts/application.js
+++ assets/coffescripts/application.js
@@ -1,1 +1,34 @@
-console.log('loaded');
+$(function() {
+  var myQr = $('#qr_input');
+  myQr.on('change', sendQr);
+
+  function sendQr() {
+    var formData = new FormData($('form')[0]);
+    $.ajax({
+      url: '/qr',
+      type: 'POST',
+      // xhr: function() {
+      //   var myXhr = $.ajaxSettings.xhr();
+      //   if(myXhr.upload){
+      //     myXhr.upload.addEventListener('progress', progressHandlingFunction, false);
+      //   }
+      //   return myXhr;
+      // },
+      // Form data
+      data: formData,
+      // Options
+      cache: false,  // not necessary
+      contentType: false, // necessary
+      processData: false // necessary
+    }).done(
+      function(data) {
+        $('#result').html(data);
+      }
+    ).fail(
+      function(error) {
+        console.log(e = error);
+      }
+    );
+  }
+
+}); // End jQuery
